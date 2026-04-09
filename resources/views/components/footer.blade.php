b9b28762f1e54c224a617903d63f131f21494e3b
Reyes Kevin <102432119+ReyesKevinJ@users.noreply.github.com>
2026-04-09 01:19:26 -0300

    Refactor code structure for improved readability and maintainability

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,114 @@
+<div class="mx-auto w-full max-w-screen-2xl p-4 py-6 lg:py-8 2xl:px-10">
+    <div class="md:flex md:justify-between">
+        <div class="mb-6 md:mb-0">
+            <a href="/" class="flex items-center">
+                <svg fill="#704214" class="h-10 w-10" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512.329 512.329" xml:space="preserve">
+                    <g>
+                        <g>
+                            <g>
+                                <path d="M166.927,256h67.904c11.782,0,21.333-9.551,21.333-21.333v-67.904C256.165,74.661,181.504,0,89.402,0H21.498
+                    C9.716,0,0.165,9.551,0.165,21.333v67.904C0.165,181.339,74.825,256,166.927,256z M213.498,166.763v16.401L73.001,42.667h16.401
+                    C157.94,42.667,213.498,98.225,213.498,166.763z M42.831,72.837l140.497,140.497h-16.401
+                    c-68.538,0-124.096-55.558-124.096-124.096V72.837z" />
+                                <path d="M490.831,106.667h-25.237c-92.102,0-166.763,74.661-166.763,166.763v25.237c0,11.782,9.551,21.333,21.333,21.333h25.237
+                    c92.102,0,166.763-74.661,166.763-166.763V128C512.165,116.218,502.613,106.667,490.831,106.667z M469.498,153.237
+                    c0,68.538-55.558,124.096-124.096,124.096h-3.904v-3.904c0-68.538,55.558-124.096,124.096-124.096h3.904V153.237z" />
+                                <path d="M298.723,350.69c-69.792-69.792-182.946-69.792-252.759-0.003l-31.107,31.107c-12.97,13.03-12.97,34.057-0.034,47.053
+                    l31.138,31.139c69.816,69.792,182.97,69.792,252.762,0l31.104-31.104c13.003-13.003,13.003-34.084,0-47.087L298.723,350.69z
+                    M268.553,380.86l3.14,3.14H72.99l3.14-3.14C129.28,327.729,215.422,327.729,268.553,380.86z M76.128,429.813l-3.146-3.146
+                    h198.719l-3.149,3.149C215.422,482.946,129.28,482.946,76.128,429.813z" />
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+                <span class="font-sans-C italic self-center text-xl text-heading font-semibold whitespace-nowrap">AROMA MOLIDO</span>
+            </a>
+        </div>
+        <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div>
+                <h2 class="mb-6 text-sm font-semibold text-heading uppercase">Resources</h2>
+                <ul class="text-body font-medium">
+                    <li class="mb-4">
+                        <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+                    </li>
+                    <li>
+                        <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h2 class="mb-6 text-sm font-semibold text-heading uppercase">Follow us</h2>
+                <ul class="text-body font-medium">
+                    <li class="mb-4">
+                        <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
+                    </li>
+                    <li>
+                        <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h2 class="mb-6 text-sm font-semibold text-heading uppercase">Legal</h2>
+                <ul class="text-body font-medium">
+                    <li class="mb-4">
+                        <a href="#" class="hover:underline">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline">Terms &amp; Conditions</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <hr class="my-6 border-default sm:mx-auto lg:my-8" />
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <span class="text-sm text-body sm:text-center">© 2023 <a href="https://flowbite.com/"
+                class="hover:underline">Flowbite™</a>. All Rights Reserved.
+        </span>
+        <div class="flex mt-4 sm:justify-center sm:mt-0">
+            <a href="#" class="text-body hover:text-heading">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd"
+                        d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">Facebook page</span>
+            </a>
+            <a href="#" class="text-body hover:text-heading ms-5">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    fill="currentColor" viewBox="0 0 24 24">
+                    <path
+                        d="M18.942 5.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.586 11.586 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3 17.392 17.392 0 0 0-2.868 11.662 15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.638 10.638 0 0 1-1.706-.83c.143-.106.283-.217.418-.331a11.664 11.664 0 0 0 10.118 0c.137.114.277.225.418.331-.544.328-1.116.606-1.71.832a12.58 12.58 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM8.678 14.813a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.929 1.929 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
+                </svg>
+                <span class="sr-only">Discord community</span>
+            </a>
+            <a href="#" class="text-body hover:text-heading ms-5">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    fill="currentColor" viewBox="0 0 24 24">
+                    <path
+                        d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z" />
+                </svg>
+                <span class="sr-only">Twitter page</span>
+            </a>
+            <a href="#" class="text-body hover:text-heading ms-5">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd"
+                        d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">GitHub account</span>
+            </a>
+            <a href="#" class="text-body hover:text-heading ms-5">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd"
+                        d="M12 2a10 10 0 1 0 10 10A10.009 10.009 0 0 0 12 2Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.093 20.093 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM10 3.707a8.82 8.82 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.755 45.755 0 0 0 10 3.707Zm-6.358 6.555a8.57 8.57 0 0 1 4.73-5.981 53.99 53.99 0 0 1 3.168 4.941 32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.641 31.641 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM12 20.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 15.113 13a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">Dribbble account</span>
+            </a>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,121 @@
+<nav class="bg-white fixed w-full z-20 top-0 start-0 border-b border-default">
+    <div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4 2xl:px-10">
+        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <!-- <svg fill="#704214" class="h-10 w-10" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512.329 512.329" xml:space="preserve">
+                <g>
+                    <g>
+                        <g>
+                            <path d="M166.927,256h67.904c11.782,0,21.333-9.551,21.333-21.333v-67.904C256.165,74.661,181.504,0,89.402,0H21.498
+                    C9.716,0,0.165,9.551,0.165,21.333v67.904C0.165,181.339,74.825,256,166.927,256z M213.498,166.763v16.401L73.001,42.667h16.401
+                    C157.94,42.667,213.498,98.225,213.498,166.763z M42.831,72.837l140.497,140.497h-16.401
+                    c-68.538,0-124.096-55.558-124.096-124.096V72.837z" />
+                            <path d="M490.831,106.667h-25.237c-92.102,0-166.763,74.661-166.763,166.763v25.237c0,11.782,9.551,21.333,21.333,21.333h25.237
+                    c92.102,0,166.763-74.661,166.763-166.763V128C512.165,116.218,502.613,106.667,490.831,106.667z M469.498,153.237
+                    c0,68.538-55.558,124.096-124.096,124.096h-3.904v-3.904c0-68.538,55.558-124.096,124.096-124.096h3.904V153.237z" />
+                            <path d="M298.723,350.69c-69.792-69.792-182.946-69.792-252.759-0.003l-31.107,31.107c-12.97,13.03-12.97,34.057-0.034,47.053
+                    l31.138,31.139c69.816,69.792,182.97,69.792,252.762,0l31.104-31.104c13.003-13.003,13.003-34.084,0-47.087L298.723,350.69z
+                    M268.553,380.86l3.14,3.14H72.99l3.14-3.14C129.28,327.729,215.422,327.729,268.553,380.86z M76.128,429.813l-3.146-3.146
+                    h198.719l-3.149,3.149C215.422,482.946,129.28,482.946,76.128,429.813z" />
+                        </g>
+                    </g>
+                </g>
+            </svg> -->
+            <svg class="h-10 w-10" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 512 512" xml:space="preserve">
+                <g>
+                    <path style="fill:#CEA67B;" d="M392.835,512h-273.62c-9.754,0-17.655-7.901-17.655-17.655v-52.966
+		c-57.98-135.583-59.825-245.822,0-370.759V17.655C101.56,7.901,109.46,0,119.215,0h273.62c9.754,0,17.655,7.901,17.655,17.655
+		v52.966c59.833,130.216,57.838,240.675,0,370.759v52.966C410.49,504.099,402.589,512,392.835,512" />
+                    <g>
+                        <path style="fill:#986632;" d="M410.49,70.621H101.56V17.655C101.56,7.901,109.46,0,119.215,0h273.62
+			c9.754,0,17.655,7.901,17.655,17.655V70.621z" />
+                        <path style="fill:#986632;" d="M392.835,512h-273.62c-9.754,0-17.655-7.901-17.655-17.655v-52.966h308.93v52.966
+			C410.49,504.099,402.589,512,392.835,512" />
+                        <path style="fill:#986632;" d="M101.539,70.657c-59.798,124.902-57.953,235.123,0,370.67
+			C159.351,311.279,161.346,200.837,101.539,70.657" />
+                        <path style="fill:#986632;" d="M410.482,70.621c-59.798,124.902-57.953,235.123,0,370.67
+			C468.294,311.243,470.289,200.801,410.482,70.621" />
+                    </g>
+                    <path style="fill:#291209;" d="M204.878,326.621h-15.086v-15.086c0-64.795,52.533-117.327,117.327-117.327h15.086v15.086
+		C322.205,274.088,269.672,326.621,204.878,326.621" />
+                    <path style="fill:#A87153;" d="M322.206,194.207h-9.331c-10.558,30.694-24.876,50.82-63.753,62.146
+		c-35.866,10.443-49.329,22.987-58.739,43.467c-0.38,3.858-0.591,7.759-0.591,11.714v15.086h9.304
+		c10.443-31.267,16.622-42.161,54.952-53.319c36.838-10.717,55.57-29.396,67.663-54.148c0.265-3.257,0.494-6.532,0.494-9.86V194.207
+		z" />
+                </g>
+            </svg>
+            <span class="font-sans-C italic self-center text-xl text-heading font-semibold whitespace-nowrap">AROMA MOLIDO</span>
+        </a>
+        <button data-collapse-toggle="navbar-dropdown" type="button"
+            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary"
+            aria-controls="navbar-dropdown" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" />
+            </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+            <ul
+                class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-neutral-primary">
+                <li>
+                    <a href="#"
+                        class="block py-2 px-3 text-white bg-brand rounded md:bg-transparent md:text-fg-brand md:p-0"
+                        aria-current="page">Home</a>
+                </li>
+                <li>
+                    <button id="dropdownNvbarButton" data-dropdown-toggle="dropdownNavbar"
+                        class="flex items-center justify-between w-full py-2 px-3 rounded font-medium text-heading md:w-auto hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0">
+                        Dropdown
+                        <svg class="w-4 h-4 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                            height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m19 9-7 7-7-7" />
+                        </svg>
+                    </button>
+                    <!-- Dropdown menu -->
+                    <div id="dropdownNavbar"
+                        class="z-10 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44">
+                        <ul class="p-2 text-sm text-body font-medium" aria-labelledby="dropdownNvbarButton">
+                            <li>
+                                <a href="#"
+                                    class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Settings</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Earnings</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Sign
+                                    out</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="#"
+                        class="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Services</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Pricing</a>
+                </li>
+                <li>
+                    <a href="{{route('contact')}}"
+                        class="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>
+
+        </div>
+    </div>
+</nav>
